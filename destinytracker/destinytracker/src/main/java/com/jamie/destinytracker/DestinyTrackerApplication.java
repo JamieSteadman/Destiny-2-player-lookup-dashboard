ddf41847533ac90582a90c7101dff4fab5b2cead
JamieSteadman <jamie.nune@gmail.com>
2025-12-26 17:04:03 -0500

    First commit

---
+++ destinytracker/destinytracker/src/main/java/com/jamie/destinytracker/DestinyTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.jamie.destinytracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestinyTrackerApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(DestinyTrackerApplication.class, args);
+	}
+
+}
---
+++ destinytracker/destinytracker/src/test/java/com/jamie/destinytracker/DestinyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamie.destinytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DestinyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
